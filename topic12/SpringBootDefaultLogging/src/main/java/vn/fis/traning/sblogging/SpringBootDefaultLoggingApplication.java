ab3184d04de77f95537d5a6b1929347d1964990e
ThanhTran <tuanthanh20111998@gmail.com>
2022-03-23 20:27:05 +0700

    Thực hành sử dụng logging trong spring boot

---
+++ topic12/SpringBootDefaultLogging/src/main/java/vn/fis/traning/sblogging/SpringBootDefaultLoggingApplication.java
@@ -0,0 +1,13 @@
+package vn.fis.traning.sblogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDefaultLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDefaultLoggingApplication.class, args);
+	}
+
+}
