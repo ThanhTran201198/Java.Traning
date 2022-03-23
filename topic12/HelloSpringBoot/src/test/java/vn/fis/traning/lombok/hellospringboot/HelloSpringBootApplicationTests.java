2ce20aed23af4ffeceda771f38a0a8e5fdff98ef
ThanhTran <tuanthanh20111998@gmail.com>
2022-03-23 17:50:12 +0700

    Thực hành bắt đầu với spring boot

---
+++ topic12/HelloSpringBoot/src/test/java/vn/fis/traning/lombok/hellospringboot/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.fis.traning.lombok.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
