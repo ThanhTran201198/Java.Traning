befc866d630180b726944448df2df2e8e509879e
ThanhTran <tuanthanh20111998@gmail.com>
2022-03-24 19:03:59 +0700

    Thực hành Spring Mail

---
+++ topic12/email-demo/src/main/java/vn/fis/traning/config/MyConstants.java
@@ -0,0 +1,14 @@
+package vn.fis.traning.config;
+
+public class MyConstants {
+
+    // Replace with your email here:  
+    public static final String MY_EMAIL = "tuanthanh20111999@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "";
+
+    // And receiver!
+    public static final String FRIEND_EMAIL = "tuanthanh20111999@gmail.com";
+
+}
